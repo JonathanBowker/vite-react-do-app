8f7bc37359bd489b4af7d78eaedf1be173638016
Jonathan Bowker <j.b.bowker@gmail.com>
2026-01-29 15:02:36 +0000

    Revert "Enlarge back arrow and lighten stroke"
    This reverts commit 34b2645f849ea9b234384fc20974c8444911b72e.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -184,10 +184,10 @@ export default function LoginPage() {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  strokeWidth="1.25"
+                  strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  className="h-20 w-20"
+                  className="h-16 w-16"
                 >
                   <path d="M15 18l-6-6 6-6" />
                 </svg>
